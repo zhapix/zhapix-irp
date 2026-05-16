e616690b84da03493c51a9b6e11c7ebcc3e52d2d
Vijayan T <69061573+vijayancpt@users.noreply.github.com>
2026-05-16 19:42:05 +0530

    Updates

---
+++ assets/Employee-Profile-BCuT8E1I.js
@@ -0,0 +1,1 @@
+const e=[{email:"email@example.com",firstName:"John",lastName:"Doe Test",displayName:"John Doe Test",photoUrl:"/assets/mentor-photo.png",gender:"M",empId:"ZX1000"}];export{e as default};
